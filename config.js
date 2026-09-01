0f4ba8384fba6f05fa47df96aad6503e373fc613
Maria-Willis <maria@bullamonplains.com>
2026-09-01 14:09:47 +1000

    Change NEXT_PUBLIC_SUPABASE_URL to window.SUPABASE_URL

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-NEXT_PUBLIC_SUPABASE_URL=https://vszjwtpmgslvpcxfkzve.supabase.co
+window.SUPABASE_URL=https://vszjwtpmgslvpcxfkzve.supabase.co  
 NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY=sb_publishable_UgiN3UialubFW7pc3j3KNA_ulEUJf2C
